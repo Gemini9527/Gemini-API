869acbbc572e38a158c05f7bcbe9f468c8df256f
Skarsh <skarshaug.thomas@gmail.com>
2020-11-14 17:17:07 +0100

    Comments

--- Protobuf/generate.sh
+++ Protobuf/generate.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
+# NOTE, THIS IS ONLY SUPPOSED TO BE RUN
+# FROM GEMINI-CORE IN ITS CURRENT STATE.
+
 # Generating the csharp protobuf and grpc files.
 # Need to check wheter the directory already exists
-OUTDIR=../Autoferry/Assets/Networking/ProtobufFiles/$1
+#OUTDIR=../Autoferry/Assets/Networking/ProtobufFiles/$1
+
+# TODO: SHOULD BE CHANGED TO TAKE IN PATH AS ARG
+OUTDIR=../../Networking/ProtobufFiles/$1
+
+# TODO: THIS DOES NOT WORK, SHOULD BE CHANGED TO TAKE IN PATH AS ARG.
 PYTHON_OUTDIR=../Clients/PythonClients/
+
 SRC_DIR=ProtoFiles/
 if [ $# -eq 0 ]; then
     echo "Name of protobuf file not given"
@@ -22,11 +31,12 @@ else
     ./Plugins/protoc -I=$SRC_DIR --csharp_out=$OUTDIR/ $SRC_DIR/$1/$1.proto --grpc_out=$OUTDIR/ --plugin=protoc-gen-grpc=Plugins/grpc_csharp_plugin.exe
 fi
 
+# TODO: As with then new changes, generating source files for Python should not be done here
 
-# Generating the python protobuf and grpc files.
-# Here one must provide the name of the protobuf file.
-# This is assumed that host OS is Windows
-# Check if second argument is given
+#Generating the python protobuf and grpc files.
+#Here one must provide the name of the protobuf file.
+#This is assumed that host OS is Windows
+#Check if second argument is given
 if [ $# -ge 2 ]; then
     if [ $2 == "python" ]; then
 
